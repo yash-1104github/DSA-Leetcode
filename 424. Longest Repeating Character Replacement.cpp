1596967414a1f40416078f68ae1264b8e8dd0adc
Yash Gupta <gyash1104@gmail.com>
2024-06-25 01:27:25 +0530

    Create 424. Longest Repeating Character Replacement.cpp

---
+++ 424. Longest Repeating Character Replacement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l =0,r=0,maxlen=0,maxfreq=0;
+        int hash[26] = {0}; 
+        
+     while(r<s.size()){
+             hash[s[r]-'A']++;
+             maxfreq = max(maxfreq,hash[s[r]-'A']);
+                if(r-l+1 - maxfreq>k){
+                   hash[s[l]-'A']--;
+                    l++;
+                }
+                if(r-l+1 -maxfreq <=k){
+                    maxlen = max(maxlen,r-l+1);
+                }
+                r++;
+            }
+        
+        return maxlen;
+    }
+};
+ 
