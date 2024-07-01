31dc8fc4f171efe92524b5534876db3bf056b833
Yash Gupta <gyash1104@gmail.com>
2024-07-01 18:13:10 +0530

    Create 1550. Three Consecutive Odds.cpp

---
+++ 1550. Three Consecutive Odds.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size(),cnt = 0;
+        for(int i = 0;i< n;i++){
+                if(arr[i] % 2 == 1){
+                    cnt++;
+                }
+                else cnt = 0 ;
+            if(cnt == 3) return true;
+         }
+        return false;
+    }
+};
