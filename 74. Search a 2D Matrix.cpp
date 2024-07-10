d70233b3da308027f9d4e45ee351e05bbfd60686
Yash Gupta <gyash1104@gmail.com>
2024-07-11 02:00:00 +0530

    Update 74. Search a 2D Matrix.cpp

--- 74. Search a 2D Matrix.cpp
+++ 74. Search a 2D Matrix.cpp
@@ -1,4 +1,19 @@
-
+TC = O(log m*n)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size() , n = matrix[0].size();
+        int low = 0 , high = m * n -1 ;
+        while(low <= high){
+            int mid = (low + high) /2;
+            int row = mid / n , coloum = mid % n; 
+            if(matrix[row][coloum] == target) return true;
+            else  if (matrix[row][coloum] < target) low = mid +1 ;
+            else high = mid  - 1;
+        }
+        return false;
+    }
+};
 TC = O (n * logm )
 class Solution {
 private: 
