f54ccd395c9bdcb108798dde1fc65f6603ecbae1
Yash Gupta <gyash1104@gmail.com>
2024-07-10 19:29:28 +0530

    1598. Crawler Log Folder.cpp

--- 1598. Crawler Log Folder.cpp
+++ 1598. Crawler Log Folder.cpp
@@ -13,3 +13,19 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int count = 0 , n = logs.size();
+        for(int i = 0; i< n ; i++){
+            if(logs[i] == "../"){
+                if(count > 0 ) count-- ;
+             }
+             else if (logs[i] != "./"){
+                count++ ;
+            }
+        }
+        return count;
+    }
+};
