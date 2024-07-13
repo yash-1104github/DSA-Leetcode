5aa8b2fe269ff83512bb41c013c901c7eec96c7d
Yash Gupta <gyash1104@gmail.com>
2024-07-13 18:40:32 +0530

    Create 2942. Find Words Containing Character.cpp

---
+++ 2942. Find Words Containing Character.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int>ans;
+        int n = words.size();
+        for( int i = 0 ; i < n ; i++) {
+              for(auto c : words[i]){
+                  if(x == c) {
+                      ans.push_back(i);
+                      break;
+                  }
+              }
+        }
+        return ans;
+    }
+};
