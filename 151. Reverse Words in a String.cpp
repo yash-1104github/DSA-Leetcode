a12cde123931a8223e4656fabe25192b8e31a3fc
Yash Gupta <gyash1104@gmail.com>
2024-07-14 00:24:20 +0530

    Create 151. Reverse Words in a String.cpp

---
+++ 151. Reverse Words in a String.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin() , s.end()) ;
+        int l = 0 , r = 0 , n = s.size();
+        int i = 0;
+        while(i < n){
+            while( i<n && s[i] != ' '){
+                s[r++] = s[i++];
+            }
+            if(l < r){
+                reverse(s.begin()+l , s.begin()+r);
+                s[r] = ' ';
+                r++;
+                l = r;
+            }
+            i++ ;
+        }
+        s = s.substr(0,r-1);
+        return s;
+    }
+};
