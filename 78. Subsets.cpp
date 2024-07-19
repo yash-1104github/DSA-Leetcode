66797aab4728e663a6a53ff03bb5f10a1c0c7579
Yash Gupta <gyash1104@gmail.com>
2024-07-20 00:59:06 +0530

    Create 78. Subsets.cpp

---
+++ 78. Subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+     vector<vector<int>>result;
+     void solve(vector<int>& nums , vector<int>& temp , int i){
+        int n = nums.size();
+          if(i >= n ){
+            result.push_back(temp);
+              return;
+        }
+        temp.push_back(nums[i]);
+        solve(nums , temp , i+1);
+        temp.pop_back();
+        solve(nums , temp , i+1); 
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>temp;
+         solve(nums , temp , 0);
+        return result;
+    }
+};
