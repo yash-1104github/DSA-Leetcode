a239c667db88517975790d55ba8b15a0c95e4850
Yash Gupta <gyash1104@gmail.com>
2024-07-22 06:09:42 +0530

    Create 40. Combination Sum II.cpp

---
+++ 40. Combination Sum II.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public: 
+    void solve(vector<int>& candidates , vector<int>& temp , int target, int sum , int ind , vector<vector<int>>& ans){
+     int n  = candidates.size();
+        if(sum > target ){
+        return;
+    }
+    
+    if(sum == target ){
+        ans.push_back(temp);
+        return ;
+    }
+    
+    for(int i = ind ; i< n ; i++){
+        
+            temp.push_back(candidates[i]);
+            sum += candidates[i];
+         // i = ind to make it combination and prevent from permutation
+            solve(candidates, temp, target, sum, i, ans);
+        
+            temp.pop_back();
+            sum -= candidates[i];
+        }   
+  }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n = candidates.size();
+         vector<vector<int>>ans;
+        vector<int>temp;
+        solve(candidates, temp, target, 0, 0, ans);      
+        return ans;
+    }
+};
