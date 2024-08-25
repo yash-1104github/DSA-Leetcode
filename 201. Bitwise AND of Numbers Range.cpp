018163c9e21808b259db46bc32f99280a02d1d86
Yash Gupta <gyash1104@gmail.com>
2024-08-25 17:59:21 +0530

    Update 201. Bitwise AND of Numbers Range.cpp

--- 201. Bitwise AND of Numbers Range.cpp
+++ 201. Bitwise AND of Numbers Range.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
          int shiftCnt = 0;
+        //To make the left and right value eqaul becoz AND only give one when 1 & 1 
         while(left != right){
-           left =   left >> 1 ;
-           right =    right >> 1;
+           left =   left >> 1 ;//deleteting non equal part
+           right =    right >> 1;//deleteting non equal part
             shiftCnt++;
         }
+        // left shift to place zeros in the digits which is removed 
         return (left << shiftCnt);
     }
 };
