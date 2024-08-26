88d6c517b53c84ffd9cf5cde9aedaf93481efd25
Yash Gupta <gyash1104@gmail.com>
2024-08-26 14:38:20 +0530

    Update 94. Binary Tree Inorder Traversal.cpp

--- 94. Binary Tree Inorder Traversal.cpp
+++ 94. Binary Tree Inorder Traversal.cpp
@@ -1,4 +1,19 @@
 class Solution {
+  public:
+        vector<int>temp;
+        void inorder(Node* root){
+        if(root == NULL) return ;
+        inorder(root->left);
+        temp.push_back(root->data);
+        inorder(root->right);
+        }    
+        vector<int>inOrder(Node* root) {
+          inorder(root);
+          return temp;
+    }
+};
+
+class Solution {
 public:
     vector<int>ans;
     vector<int> inorderTraversal(TreeNode* root) {
