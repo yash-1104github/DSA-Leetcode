1664de9bc358f96ebac3e2572eb14e9acd2c0cb2
Yash Gupta <gyash1104@gmail.com>
2024-08-26 14:56:12 +0530

    Update 145. Binary Tree Postorder Traversal.cpp

--- 145. Binary Tree Postorder Traversal.cpp
+++ 145. Binary Tree Postorder Traversal.cpp
@@ -1,3 +1,17 @@
+void postorder(Node* root , vector<int>& temp){
+    if(root == nullptr) return ;
+    postorder(root->left,temp);
+    postorder(root->right,temp);
+    temp.push_back(root->data);
+}
+vector <int> postOrder(Node* root)
+{
+  // Your code here
+  vector<int>temp;
+  postorder(root,temp);
+  return temp;
+}
+
 class Solution {
 public:
     vector<int>ans;
