c3e086e7377d5b538a4d5983c9b102b6578b1b44
Yash Gupta <gyash1104@gmail.com>
2024-09-27 02:31:33 +0530

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -17,6 +17,44 @@ public:
 };
 
 
+//wrong ans for case [3,3]  tar = 6 becoz both 3 will mp to same idx
+ class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      
+        int n = nums.size();
+        unordered_map<int,int>mp;
+        for(int i = 0 ; i < n ; i++){
+             mp[nums[i]] = i;
+        }
+        sort(nums.begin(),nums.end());
+        int i = 0 , j = n-1;
+        
+        while(i < j){
+            int sum = nums[i] + nums[j];
+            if(sum > target){
+                 j--;
+            }
+            else if(sum == target){
+                 break;
+            }
+             else {
+                 i++;
+             } 
+        }
+          vector<int>ans;
+            
+            if(mp.find(nums[i]) != mp.end() ){
+                 ans.push_back(mp[nums[i]]);
+            }
+            if(mp.find(nums[j]) != mp.end()){
+                 ans.push_back(mp[nums[j]]);
+            }
+
+        
+        return ans;
+    }
+};
 Brute-force approach:
 class Solution {
 public:
