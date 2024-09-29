6855a975424f880e761171257f5365b3304d80c7
Yash Gupta <gyash1104@gmail.com>
2024-09-29 22:36:39 +0530

    Update 13. Roman to Integer.cpp

--- 13. Roman to Integer.cpp
+++ 13. Roman to Integer.cpp
@@ -1,19 +1,29 @@
 class Solution {
-public:
-    int romanToInt(string s) {
-        unordered_map<char,int>mp =
-        {{'I' , 1},{'V', 5},{'X' , 10},{'L' , 50},{'C' , 100},{'D', 500},{'M', 1000}};
+  public:
+    int romanToDecimal(string &str) {
+        // code here
+        unordered_map<char,int>mp = {
+            {'I' , 1},
+            {'V' , 5},
+            {'X' , 10},
+            {'L' , 50},
+            {'C' , 100},
+            {'D' , 500},
+            {'M' , 1000},     
+        };
         
-        int n  = s.size(),total = 0;
+        int n = str.size(), ans = 0;
         
         for(int i = 0 ; i < n; i++){
-            if( i + 1 < n && mp[s[i]] < mp[s[i+1]] ){
-                total -= mp[s[i]];
+            
+            if(mp[str[i]] < mp[str[i+1]]){
+                ans += mp[str[i+1]] - mp[str[i]];
+                i = i+1;
             }
             else{
-                total += mp[s[i]];
-            }
+                 ans +=  mp[str[i]];
+            }     
         }
-        return total;
+       return ans;    
     }
 };
