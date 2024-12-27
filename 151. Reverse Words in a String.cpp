01837333cdbee5b1a7021113fe2443bf5dcae1fd
Yash Gupta <gyash1104@gmail.com>
2024-12-28 01:54:46 +0530

    Update 151. Reverse Words in a String.cpp

--- 151. Reverse Words in a String.cpp
+++ 151. Reverse Words in a String.cpp
@@ -1,4 +1,41 @@
+class Solution {
+public:
+    string reverseWords(string s) {    
+           
+        string ans = "";
+        int i = s.size()-1;
+        
+
+        while( i >= 0){  
+                
+                while(i >= 0 && s[i] == ' '){
+                    i--;
+                }
+
+                if(i < 0) break;
+
+                int j = i;
+                
+                while(i >= 0 && s[i] != ' '){
+                     i--;
+                }
+              
+                ans += s.substr(i+1,j-i);
+
+               ans += " ";
+           
+        }   
+
+        string res = ans.substr(0 , ans.size()-1);
+
+         return res ;
+    }
+};
+
+
 //leetcode
+
+
 class Solution {
 public:
     string reverseWords(string s) {
