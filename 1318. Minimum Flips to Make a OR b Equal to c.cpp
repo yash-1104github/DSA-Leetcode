365b163fdd02824e7208d75254f4462a8a8c8a2a
Yash Gupta <gyash1104@gmail.com>
2025-01-04 03:24:42 +0530

    Create 1318. Minimum Flips to Make a OR b Equal to c.cpp

---
+++ 1318. Minimum Flips to Make a OR b Equal to c.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+         int flip = 0;
+
+         while(a !=  0 || b!= 0 || c!= 0){
+                       //rightmost bit
+                     if((c&1) == 1){                          
+                           if((a&1) == 0 && (b&1)==0) {
+                                          flip++;
+                           }
+                      }
+                     else {
+
+                          if((a&1) == 1) flip++;
+                          if((b&1) == 1) flip++; 
+                     }
+            //right-shift
+            a>>=1;
+            b>>=1;
+            c>>=1;
+         }
+return flip;
+    }
+};
