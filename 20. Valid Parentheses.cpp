747fc5f33287ecacb7b0b7280c4fe98b570a41f7
Yash Gupta <gyash1104@gmail.com>
2025-01-15 00:19:12 +0530

    Update 20. Valid Parentheses.cpp

--- 20. Valid Parentheses.cpp
+++ 20. Valid Parentheses.cpp
@@ -19,3 +19,45 @@ public:
         return st.empty(); 
     }
 };
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        int n = s.size();
+        
+        if(n%2==1) return false;
+        
+        stack<char>st;
+ 
+  
+       for(int i = 0  ; i < n ; i++){ 
+            
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){     
+                   st.push(s[i]);
+            }
+            else{
+            if(st.empty())  return false;
+            char  ch = st.top();
+
+             if(s[i] == ')' && ch == '(' ||
+                s[i] == '}' && ch == '{' ||
+                s[i] == ']' && ch == '['
+                ){
+                st.pop();
+             }
+
+             else{
+                return false;
+             }
+                 
+            }         
+        }
+
+        if(st.empty()) return true;
+
+     return false;
+
+    }
+};
