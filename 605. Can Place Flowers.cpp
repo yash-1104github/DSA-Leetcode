fdd7e5a17d31f940a0b469a63a79493f1ebec01b
Yash Gupta <gyash1104@gmail.com>
2025-04-07 17:52:18 +0530

    Update 605. Can Place Flowers.cpp

--- 605. Can Place Flowers.cpp
+++ 605. Can Place Flowers.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-         
-         if(n == 0) return true;
+        int m = flowerbed.size();
+         if(m==1 && n==0)return true;
+         if(m==1 && n > 1)return false;
+         if(m==1 && n==1 && flowerbed[0] == 0) return true;
+         if(m==1 && n==1 && flowerbed[0] == 1) return false;
 
-         int m = flowerbed.size();
+         if(flowerbed[0] == 0 && flowerbed[1] == 0){
+            flowerbed[0] = 1;
+            n--;
+         }
 
-           for(int i = 0 ; i < m ;i++){
+         for(int i = 1; i < m-1; i++){
+             if(flowerbed[i] ==0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0){
+                flowerbed[i] = 1;
+                n--;
+             }
+        }
 
-                   if(flowerbed[i] == 0){
+        if(flowerbed[m-1] == 0 && flowerbed[m-2] == 0) {
+            n--;
+        }
 
-                 bool leftkhali = ( i == 0 || flowerbed[i-1]  == 0 );
-                 bool rightkhali = (i == m-1 || flowerbed[i+1]  == 0);
-         
-                if(leftkhali && rightkhali){
-                    flowerbed[i] = 1;
-                    n--;    
-                }
-                     
-                 if(n == 0 ) return true;
-            }
-        }  
-             return false;
+      if(n <= 0) return true;
+      return false;
     }
 };
